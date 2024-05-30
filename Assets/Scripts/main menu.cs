03df06080d78e28e4a817ce5c8441afadd5b5b69
Jun-Hyuk Park <wldydEnajd>
2024-05-30 20:54:20 +0900

    Add UI

---
+++ Assets/Scripts/main menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void SingleBtn()
+    {
+        SceneManager.LoadScene("SinglePlay");
+    }
+    public void MultiBtn()
+    {
+        SceneManager.LoadScene("MultiPlay");
+    }
+    public void SettingBtn()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
---
+++ Assets/Scripts/single play.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SinglePlay : MonoBehaviour
+{
+    public void BackBtn()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+}
